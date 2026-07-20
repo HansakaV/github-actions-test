1c40fd7ac620643c701749d8d4b0826d917c97a3
Mahesh Hansaka <maheshhansaka628@gmail.com>
2026-07-20 16:17:09 +0530

    feat:configure DbConeection & loggers

---
+++ data/AppDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace github_actions_test.data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
